ff3263b3f304ca4634765264d1a4170f334b7fa9
German Voytekhovich <ueuknjg@gmail.com>
2019-12-21 18:25:12 +0300

    added paginate services

--- src/services/structuredData.js
+++ src/services/structuredData.js
@@ -58,6 +58,41 @@ class StructuredData {
 			num_on_page,
 			total_records
 		}
+	};
+
+	withPaginationNew = Model => {
+		const pagination = async ({ page = null, num_on_page = null, ...params }) => {
+			const options = Object.assign({}, params);
+
+			if (!page || !num_on_page) {
+				return Model.findAll(options);
+			}
+
+			const countOptions = Object.keys(options).reduce((acc, key) => {
+				if (!['order', 'attributes', 'include'].includes(key)) {
+					acc[key] = options[key]
+				}
+
+				return acc
+			}, {});
+
+			const total_records = await Model.count(countOptions);
+
+			const page_count = Math.ceil(total_records / num_on_page);
+
+			options.limit = num_on_page;
+			options.offset = num_on_page * (page - 1);
+
+			if (params.order) options.order = params.order;
+
+			const data = await Model.findAll(options);
+
+			return { data, page_count, total_records, num_on_page: Number(num_on_page), page: Number(page) }
+		};
+
+		const instanceOrModel = Model.Instance || Model;
+
+		instanceOrModel.paginate = pagination;
 	}
 }
 
